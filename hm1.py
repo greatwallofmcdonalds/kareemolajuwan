1a416499e77d0ad727a94f70da6d39f0b5247f40
User <jaydenwillivlie@gmail.com>
2026-02-07 11:31:26 +0700

    jaydenjinnggahandsome

---
+++ hm1.py
@@ -0,0 +1,9 @@
+
+
+basic_math_operator_list = ["+","-","*","/"]
+v1=int(input("please input a number for index"))
+if v1 >=0 and v1 <=3:
+    print(basic_math_operator_list[v1])
+else:
+    print("invalid number")
+    print("shut yo balllllls")
